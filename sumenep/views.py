5e10743e5a938fa77964e689ed6261dc1cd88be9
unknown <rakkakeren@gmail.com>
2022-04-17 09:38:47 +0700

    update requirements.txt and style barplot

--- sumenep/views.py
+++ sumenep/views.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
@@ -15,11 +16,18 @@ def view_form():
     st.subheader("Prediksi Status Kesehatan Keluarga")
 
     features_json = load_features()
+    feature_len = len(features_json)
     features_user = {}
     with st.form("Feature"):
-        for feature_name, values in features_json.items():
-            option = st.selectbox(label=feature_name, options=values)
-            features_user[feature_name] = option
+        col1, col2 = st.columns([6, 6])
+
+        for index, (feature_name, values) in enumerate(features_json.items()):
+            used_col = col1 if index < feature_len // 2 else col2
+
+            with used_col:
+                option = st.selectbox(label=feature_name, options=values)
+                features_user[feature_name] = option
+
         is_submit = st.form_submit_button(label="Submit")
     
     if is_submit:
@@ -34,5 +42,19 @@ def view_form():
         # Shot Probability Plot
         fig, ax = plt.subplots(figsize=(10, 5))
         ax.set_title("Probabilitas Status Kesehatan Keluarga")
-        ax.bar(x=classes, height=prob[0], width=0.4)
+        ax.set_ylim((0, 1.2))
+        ax.set_yticks([])
+
+        ## Barplot
+        cmap = plt.get_cmap("tab10")
+        barplot = ax.bar(x=classes, height=prob[0], width=0.4, color=cmap(prob[0]))
+
+        ## Annotations
+        rects = barplot.patches
+        fontdict = dict(size=12)
+        for rect in rects:
+            value = f"{rect.get_height():.2%}"
+            ax.text(x=rect.get_x() + 0.1, y=rect.get_height() + 0.05, s=value, fontdict=fontdict)
+
+        
         st.pyplot(fig)
